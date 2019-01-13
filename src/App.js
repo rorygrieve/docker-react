731b50b541e88ffec36929ef5e9a9237a7c6e886
Rory Grieve <rhrg500@gmail.com>
2019-01-13 23:00:22 +0000

    Text change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. or not really  a
+            Edit <code>src/App.js</code> and save to reload. Changed on feature
           </p>
           <a
             className="App-link"
